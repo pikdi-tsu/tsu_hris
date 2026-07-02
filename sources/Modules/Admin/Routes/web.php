4f41b5beb3c8b6373f03b438734389142e48a49e
Yoda-TSU <yodha@tsu.ac.id>
2026-07-02 10:40:39 +0700

    make menu upload absensi and create update

--- sources/Modules/Admin/Routes/web.php
+++ sources/Modules/Admin/Routes/web.php
@@ -19,6 +19,7 @@ use \Modules\Admin\Http\Controllers\MasterLemburController;
 use Modules\Admin\Http\Controllers\MasterJabatanController;
 use Modules\Admin\Http\Controllers\RiwayatJabatanController;
 use Modules\Admin\Http\Controllers\RiwayatIzinCutiController;
+use Modules\Admin\Http\Controllers\AbsensiController;
 use Illuminate\Support\Facades\Route;
 
 // Aktifkan CheckAdminRole::class di middleware jika ada dashboard users sendiri
@@ -160,5 +161,16 @@ Route::prefix('admin')->name('admin.')->middleware(['auth'])->group(function () 
             Route::get('/jsonizin', [RiwayatIzinCutiController::class, 'datatableizin'])->name('jsonizin');
             Route::get('/jsoncuti', [RiwayatIzinCutiController::class, 'datatablecuti'])->name('jsoncuti');
         });
+    });
+
+    // --- ROUTE ABSENSI ---
+    Route::prefix('absensi')->name('absensi.')->group(function () {
+        Route::get('/', [AbsensiController::class, 'index'])->name('index');
+        Route::post('/uploadexcel', [AbsensiController::class, 'simpanexcel'])->name('uploadexcel');
+        Route::get('/datatablesabsensi', [AbsensiController::class, 'datatableabsensi'])->name('datatablesabsensi');
+        Route::post('/updateperiode', [AbsensiController::class, 'update'])->name('updateperiode');
+
+        // Route::post('/edit', [AbsensiController::class, 'edit'])->name('edit');
+        // Route::post('/detail', [AbsensiController::class, 'detail'])->name('detail');
     });
 });
